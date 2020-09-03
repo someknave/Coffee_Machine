7863024685b8d40e2a8bd675c9f50f633ee5429b
Joe Levey <josephfalevey@gmail.com>
2020-09-04 03:48:03 +1000

    complete stage 2 commence stage 3

--- Coffee Machine/task/src/machine/Main.kt
+++ Coffee Machine/task/src/machine/Main.kt
@@ -1,6 +1,17 @@
 package machine
 
+import java.util.*
+
+val scanner = Scanner(System.`in`)
+
 fun main() {
+    requestSupplies()
+
+}
+
+
+
+fun makeCoffee() {
     println("Starting to make a coffee")
     println("Grinding coffee beans")
     println("Boiling water")
@@ -8,4 +19,16 @@ fun main() {
     println("Pouring coffee into the cup")
     println("Pouring some milk into the cup")
     println("Coffee is ready!")
+}
+
+fun requestSupplies() {
+    print("Write how many cups of coffee you will need:")
+    val cups = scanner.nextInt()
+    val water = cups * 200
+    val milk = cups * 50
+    val beans = cups * 15
+    println("For $cups cups of coffee you will need:")
+    println("$water ml of water")
+    println("$milk ml of milk")
+    println("$beans g of coffee beans")
 }
